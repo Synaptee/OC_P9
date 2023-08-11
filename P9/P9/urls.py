97384f73b4250eb771c7005abf55bd4debeff0e3
Synaptee <jba.munoz@gmail.com>
2023-08-11 18:18:24 +0200

    Fichier gitignore + création de la base HTML

--- P9/P9/urls.py
+++ P9/P9/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import (
     LoginView,
-
 )
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", LoginView.as_view(), name="login"),
- 
-    ),
 ]
