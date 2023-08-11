ae9735bc4e3f260e83536c41f799bff3494784b8
Synaptee <jba.munoz@gmail.com>
2023-08-11 17:20:46 +0200

    Création des modèles de la BDD

--- P9/critiques/models.py
+++ P9/critiques/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        max_length=1024, validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
+
+
+class UserFollows(models.Model):
+    Meta = models.UniqueConstraint(unique_together=["user", "followed_user"])
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following"
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by",
+    )
+
+    def __str__(self):
+        return self.user.username + " follows " + self.followed_user.username
